89d5635ccc42e901f6830e43823fda7e464aa6c9
Addison Holbrook <holbrooa@gmail.com>
2020-08-17 16:15:11 +0200

    coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+
+
+  def answer
+  @asked = params[:asked]
+  
+    if @asked ==  "I am going to work"
+      @answer = "Great!"
+      elsif @asked.include? "?"
+        @answer = "Silly question, get dressed and go to work!" 
+      else 
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    
+  end
+  
+ 
+ 
+end
